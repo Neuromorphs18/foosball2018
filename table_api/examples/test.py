0f1673262cac43db528817e2d8eff95bbea9430d
Andrew Wabnitz <andywabnitz@gmail.com>
2018-07-13 21:02:52 -0600

    New version with faster comms

---
+++ table_api/examples/test.py
@@ -0,0 +1,38 @@
+import time
+from sensiball import Sensiball
+
+def myHandlerFunc(pos):
+    print(pos)
+
+sb = Sensiball()
+sb.open(None, '/dev/cu.usbmodem1411', 115200)
+#sb.add_handler(myFunc)
+sb.enable_printout(True)
+time.sleep(1)
+
+# Calibrate
+sb.send_calibrate()
+time.sleep(2)
+
+sb.send_speeds([50,150,0,0,0,0,0,0])
+time.sleep(1)
+
+sb.send_speeds([0,0,50,150,0,0,0,0])
+time.sleep(1)
+
+sb.send_speeds([0,0,0,0,50,150,0,0])
+time.sleep(2)
+
+sb.send_speeds([0,0,0,0,-50,-150,0,0])
+time.sleep(2)
+
+sb.send_speeds([-50,-150,0,0,0,0,0,0])
+time.sleep(1)
+
+sb.send_speeds([0,0,-50,-150,0,0,0,0])
+time.sleep(1)
+
+sb.send_speeds([0,0,0,0,-50,-150,0,0])
+time.sleep(1)
+
+sb.send_halt()
