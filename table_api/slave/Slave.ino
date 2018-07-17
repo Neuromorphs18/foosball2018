907ef4879a592a069cd364f11853824086d901cb
Andrew Wabnitz <andywabnitz@gmail.com>
2018-07-16 19:30:48 -0600

    New firmware with position and kick

--- table_api/slave/Slave.ino
+++ table_api/slave/Slave.ino
@@ -3,41 +3,53 @@
 //##################################################################
 // CONFIGURATION
 //##################################################################
-#define LED_FLASH_PERIOD_MS   1000
-#define SLAVE_ADDRESS         12           // 8/10/12/14 for Goalie/Defender/Midfield/Forward
-#define SLAVE_MODE            1           // 0/1 for Translation/Rotation
-#define TRANSLATION_CAL_SPEED 120
-#define ROTATION_CAL_SPEED    110
+#define LED_FLASH_PERIOD_MS     1000
+#define SLAVE_ADDRESS           10           // 8/10/12/14 for Goalie/Defender/Midfield/Forward
+#define SLAVE_MODE              0            // 0/1 for Translation/Rotation
+#define SPEED_CAL_TRANSLATION   125
+#define SPEED_CAL_ROTATION      110
+#define SPEED_SLIDE_FAST        125
+#define SPEED_SLIDE_SLOW        100
+#define SPEED_WINDUP            220
+#define SPEED_KICK              250
+#define SPEED_RESET             100
+#define POS_WINDUP              -180  
+#define POS_KICK                80 
+#define POS_RESET               0
+#define DIFF_SLOW               200
 
-#define STATUS_OFFLINE        0x50
-#define STATUS_CRC_ERROR      0x51
-#define STATUS_UNCALIBRATED   0x52
-#define STATUS_CALIBRATED     0x53
-
-#define CMD_POKE              0xF0
-#define CMD_HALT              0xF1
-#define CMD_CALIBRATE         0xF2
-#define CMD_SET_SPEED         0xF3
-#define RESP_ACK              0xFA
-#define RESP_NACK             0xFB
+#define CMD_HALT                0xF0
+#define CMD_CALIBRATE           0xF1
+#define CMD_SLIDE               0xF2
+#define CMD_KICK                0xF3
+#define RESP_ACK                0xFA
+#define RESP_NACK               0xFB
 
 // Pins
-#define PIN_DIRECTION         8
-#define PIN_PWM               9
-#define PIN_OUTER_SWITCH      11 
-#define PIN_INNER_SWITCH      12
-#define PIN_ENCODER_FORWARD   2
-#define PIN_ENCODER_BACKWARD  3
-#define PIN_VERTICAL_SWITCH   A0
+#define PIN_DIRECTION           8
+#define PIN_PWM                 9
+#define PIN_OUTER_SWITCH        11 
+#define PIN_INNER_SWITCH        12
+#define PIN_ENCODER_FORWARD     2
+#define PIN_ENCODER_BACKWARD    3
+#define PIN_VERTICAL_SWITCH     A0
 
 //##################################################################
 // GLOBAL VARIABLES
 //##################################################################
 typedef enum
 {
-  STATE_UNCALIBRATED,
-  STATE_CALIBRATING,
-  STATE_CALIBRATED
+  STATE_IDLE,
+  STATE_CALIBRATING_1,
+  STATE_CALIBRATING_2,
+  STATE_CALIBRATING_3,
+  STATE_CALIBRATING_4,
+  STATE_SLIDE,
+  STATE_SLIDE_IN,
+  STATE_SLIDE_OUT,
+  STATE_WINDUP,
+  STATE_KICK,
+  STATE_RESET
 } state_t;
 
 typedef enum
@@ -48,13 +60,12 @@ typedef enum
 } result_t;
 
 // State variables
-volatile state_t  state          = STATE_UNCALIBRATED;
-volatile result_t result         = RESULT_NONE;
-volatile int      minimum_pulses = 0;
-volatile int      maximum_pulses = 980;
-volatile int      pulses         = 0;
-volatile bool     clockwise      = false;
-volatile byte     speed          = 0;
+volatile state_t  state           = STATE_IDLE;
+volatile result_t result          = RESULT_NONE;
+volatile int      target_pulse    = 0;
+volatile int      maximum_pulses  = 0;
+volatile int      pulses          = 0;
+volatile bool     isCalibrated    = false;
 
 //##################################################################
 // INITIALISATION
@@ -74,12 +85,11 @@ void setup()
   analogWrite(PIN_PWM, 0);
     
   Wire.begin(SLAVE_ADDRESS + SLAVE_MODE);
-  //Wire.setClock(10000);
   Wire.onReceive(receive_event);
   Wire.onRequest(request_event);
     
   attachInterrupt(0, forward_change, CHANGE);
-  attachInterrupt(1, backward_change, CHANGE);  
+  attachInterrupt(1, backward_change, CHANGE);
 }
 
 //##################################################################
@@ -87,11 +97,13 @@ void setup()
 //##################################################################
 void loop()
 {
-  static unsigned long  ledTimestamp    = 0;
-  static unsigned long  updateTimestamp = 0;
-  static bool           ledState        = true;
-  static byte           calState        = 0;
+  static unsigned long  ledTimestamp  = 0;
+  static unsigned long  dlyTimestamp  = 0;
+  static bool           ledState      = true;
+  static byte           moveState     = 0;
+  static bool           moveFast;
   unsigned long         timeNow;
+  int                   diff;
   
   timeNow = millis();
 
@@ -110,147 +122,246 @@ void loop()
   ///////////////////////////////////////////////////
   switch (state)
   {
-    case STATE_UNCALIBRATED:
+    case STATE_IDLE:
     {
+      analogWrite(PIN_PWM, 0);
       break;
     }
 
-    case STATE_CALIBRATING:
+    #if SLAVE_MODE == 0
+    case STATE_CALIBRATING_1:
     {
-      #if SLAVE_MODE == 0
-      switch (calState)
-      {
-        case 0:
-        {
-          digitalWrite(PIN_DIRECTION, HIGH);
-          analogWrite(PIN_PWM, TRANSLATION_CAL_SPEED);
-          calState++;
-          break;
-        }
-
-        case 1:
-        {
-          if (digitalRead(PIN_OUTER_SWITCH) == LOW)
-          {
-              analogWrite(PIN_PWM, 0);
-              noInterrupts();
-              maximum_pulses = pulses + 1;
-              interrupts();
-              digitalWrite(PIN_DIRECTION, LOW);
-              analogWrite(PIN_PWM, TRANSLATION_CAL_SPEED);
-              calState++;
-          }
-          break;
-        }
-
-        case 2:
-        {
-          if (digitalRead(PIN_INNER_SWITCH) == LOW)
-          {
-              analogWrite(PIN_PWM, 0);
-              noInterrupts();
-              minimum_pulses = pulses;
-              interrupts();
-              calState++;
-          }
-          break;
-        }
-
-        default:
-        {
-          calState  = 0;
-          state     = STATE_CALIBRATED;
-          break;
-        }
-      }
-      #else
-      // Forwards has a busted sensor, so ignore calibration
-      #if SLAVE_ADDRESS == 14
-      calState  = 0;
-      state     = STATE_CALIBRATED;
-      #else
-      switch (calState)
-      {
-        case 0:
-        {
-          digitalWrite(PIN_DIRECTION, HIGH);
-          analogWrite(PIN_PWM, ROTATION_CAL_SPEED);
-          calState++;
-          break;
-        }
-
-        case 1:
-        {
-          if (digitalRead(PIN_VERTICAL_SWITCH) == LOW)
-          {
-              analogWrite(PIN_PWM, 0);
-              noInterrupts();
-              pulses = 0;
-              interrupts();
-              calState++;
-          }
-          break;
-        }
-
-        default:
-        {
-          calState  = 0;
-          state     = STATE_CALIBRATED;
-          break;
-        }
-      }
-      #endif
-      #endif
+      digitalWrite(PIN_DIRECTION, LOW);
+      analogWrite(PIN_PWM, SPEED_CAL_TRANSLATION);
+      state = STATE_CALIBRATING_2;
       break;
     }
-  
-    case STATE_CALIBRATED:
-    {
-      noInterrupts();
-      bool local_clockwise  = clockwise;
-      byte local_speed      = speed;
-      interrupts();
 
-      #if SLAVE_MODE == 0
+    case STATE_CALIBRATING_2:
+    {
       if (digitalRead(PIN_INNER_SWITCH) == LOW)
       {
-        noInterrupts();
-        minimum_pulses = pulses;
-        interrupts();
-        if (!local_clockwise)
-        {
-            local_speed = 0;
-        }
+        analogWrite(PIN_PWM, 0);
+        delay(50);
+        digitalWrite(PIN_DIRECTION, HIGH);
+        analogWrite(PIN_PWM, SPEED_CAL_TRANSLATION);
+        state = STATE_CALIBRATING_3;
       }
-      else if (digitalRead(PIN_OUTER_SWITCH) == LOW)
+      break;
+    }
+      
+    case STATE_CALIBRATING_3:
+    {
+      if (digitalRead(PIN_OUTER_SWITCH) == LOW)
       {
+        analogWrite(PIN_PWM, 0);
+        delay(100);
         noInterrupts();
-        maximum_pulses = pulses + 1;
+        maximum_pulses = pulses;
         interrupts();
-        if (local_clockwise)
-        {
-            local_speed = 0;
-        }
+        digitalWrite(PIN_DIRECTION, LOW);
+        analogWrite(PIN_PWM, SPEED_CAL_TRANSLATION);
+        state = STATE_CALIBRATING_4;
       }
+      break;
+    }
+
+    case STATE_CALIBRATING_4:
+    {
+      if (digitalRead(PIN_INNER_SWITCH) == LOW)
+      {
+        analogWrite(PIN_PWM, 0);
+        delay(100);
+        isCalibrated = true;
+        noInterrupts();
+        maximum_pulses -= pulses;
+        pulses = 0;
+        interrupts();
+        state = STATE_IDLE;
+      }
+      break;
+    }
+    #else
+    case STATE_CALIBRATING_1:
+    {
+      digitalWrite(PIN_DIRECTION, HIGH);
+      analogWrite(PIN_PWM, SPEED_CAL_ROTATION);
+      state = STATE_CALIBRATING_2;
+      delay(50);
+      break;
+    }
+
+    case STATE_CALIBRATING_2:
+    {
+      if (digitalRead(PIN_VERTICAL_SWITCH) == LOW)
+      {
+        analogWrite(PIN_PWM, 0);
+        delay(100);
+        noInterrupts();
+        pulses = 0;
+        interrupts();
+        digitalWrite(PIN_DIRECTION, HIGH);
+        analogWrite(PIN_PWM, SPEED_CAL_ROTATION);
+        state = STATE_CALIBRATING_3;
+        delay(50);
+      }
+      break;
+    }
+
+    case STATE_CALIBRATING_3:
+    {
+      if (digitalRead(PIN_VERTICAL_SWITCH) == LOW)
+      {
+        analogWrite(PIN_PWM, 0);
+        delay(100);
+        isCalibrated = true;
+        noInterrupts();
+        maximum_pulses = pulses >> 1;
+        pulses = 0;
+        interrupts();
+        state = STATE_IDLE;
+      }
+      break;
+    }
+    #endif
+
+    case STATE_SLIDE:
+    {
+      
+      // Move towards inner switch
+      if (target_pulse < pulses)
+      {
+        if (moveState == 2)
+        {
+          analogWrite(PIN_PWM, 0);
+        }
+        moveState = 1;
+        digitalWrite(PIN_DIRECTION, LOW);
+        state = STATE_SLIDE_IN;
+      }
+      // Move towards outer switch
       else
       {
-        if (pulses + 1 > maximum_pulses)
+        if (moveState == 1)
         {
-            noInterrupts();
-            maximum_pulses = pulses + 1;
-            interrupts();
+          analogWrite(PIN_PWM, 0);
         }
-        else if (pulses < minimum_pulses)
-        {
-            noInterrupts();
-            minimum_pulses = pulses;
-            interrupts();
-        }
+        moveState = 2;
+        digitalWrite(PIN_DIRECTION, HIGH);
+        state = STATE_SLIDE_OUT;
       }
-      #endif
+      moveFast = true;
+      break;
+    }
 
-      digitalWrite(PIN_DIRECTION, local_clockwise ? HIGH : LOW);
-      analogWrite(PIN_PWM, local_speed);
+    case STATE_SLIDE_IN:
+    {
+        if (digitalRead(PIN_INNER_SWITCH) == LOW)
+        {
+          analogWrite(PIN_PWM, 0);
+          delay(100);
+          moveState = 0;
+          noInterrupts();
+          maximum_pulses -= pulses;
+          pulses = 0;
+          state = (state == STATE_SLIDE_IN) ? STATE_IDLE : state;
+          interrupts();
+        }
+        else
+        {
+          diff = pulses - target_pulse;
+          if (moveFast || (diff > DIFF_SLOW))
+          {
+            moveFast = false;
+            analogWrite(PIN_PWM, SPEED_SLIDE_FAST);
+          }
+          else if (diff > 0)
+          {
+            analogWrite(PIN_PWM, SPEED_SLIDE_SLOW);
+          }
+          else
+          {
+            analogWrite(PIN_PWM, 0);
+            moveState = 0;
+            noInterrupts();
+            state = (state == STATE_SLIDE_IN) ? STATE_IDLE : state;
+            interrupts();
+          }
+        }
+        break;
+    }
+    
+    case STATE_SLIDE_OUT:
+    {
+        if (digitalRead(PIN_OUTER_SWITCH) == LOW)
+        {
+          analogWrite(PIN_PWM, 0);
+          delay(100);
+          moveState = 0;
+          noInterrupts();
+          maximum_pulses = pulses;
+          state = (state == STATE_SLIDE_OUT) ? STATE_IDLE : state;
+          interrupts();
+        }
+        else
+        {
+          diff = target_pulse - pulses;
+          if (moveFast || (diff > DIFF_SLOW))
+          {
+            moveFast = false;
+            analogWrite(PIN_PWM, SPEED_SLIDE_FAST);
+          }
+          else if (diff > 0)
+          {
+            analogWrite(PIN_PWM, SPEED_SLIDE_SLOW);
+          }
+          else
+          {
+            analogWrite(PIN_PWM, 0);
+            moveState = 0;
+            noInterrupts();
+            state = (state == STATE_SLIDE_OUT) ? STATE_IDLE : state;
+            interrupts();
+          }
+        }
+        break;
+    }
+    
+    case STATE_WINDUP:
+    {
+      if (digitalRead(PIN_VERTICAL_SWITCH) != LOW)
+      {
+        digitalWrite(PIN_DIRECTION, HIGH);
+        analogWrite(PIN_PWM, SPEED_CAL_ROTATION);
+        while (digitalRead(PIN_VERTICAL_SWITCH) != LOW);
+        analogWrite(PIN_PWM, 0);
+      }
+      
+      digitalWrite(PIN_DIRECTION, LOW);
+      analogWrite(PIN_PWM, SPEED_WINDUP);
+      while (pulses >= POS_WINDUP);
+      analogWrite(PIN_PWM, 0);
+      state = STATE_KICK;
+      break;
+    }
+
+    case STATE_KICK:
+    {
+      digitalWrite(PIN_DIRECTION, HIGH);
+      analogWrite(PIN_PWM, SPEED_KICK);
+      while (pulses <= POS_KICK);
+      analogWrite(PIN_PWM, 0);
+      state = STATE_RESET;
+      break;
+    }
+
+    case STATE_RESET:
+    {
+      digitalWrite(PIN_DIRECTION, LOW);
+      analogWrite(PIN_PWM, SPEED_RESET);
+      while (pulses >= POS_RESET);
+      analogWrite(PIN_PWM, 0);
+      state = STATE_IDLE;
       break;
     }
   }
@@ -332,36 +443,35 @@ void receive_event(int len)
 
       switch (cmd[0])
       {
-        case CMD_POKE:
-        {
-          break;
-        }
-
         case CMD_HALT:
         {
-          if (state == STATE_CALIBRATING)
-          {
-            state = STATE_UNCALIBRATED;
-          }
-          clockwise = false;
-          speed     = 0;
+          state = STATE_IDLE;
           break;
         }
 
         case CMD_CALIBRATE:
         {
-          state = STATE_CALIBRATING;
+          state = STATE_CALIBRATING_1;
+          isCalibrated = false;
           break;
         }
 
-        case CMD_SET_SPEED:
+        case CMD_SLIDE:
         {
-          if (state == STATE_CALIBRATING)
+          if (isCalibrated)
           {
-            state = STATE_UNCALIBRATED;
+            state = STATE_SLIDE;
+            target_pulse = (((int)cmd[2]) << 8) | ((int)cmd[1]);
           }
-          clockwise = (cmd[1] != 0);
-          speed     = cmd[2];
+          break;
+        }
+
+        case CMD_KICK:
+        {
+          if ((state == STATE_IDLE) && isCalibrated)
+          {
+            state = STATE_WINDUP;
+          }
           break;
         }
         
@@ -384,14 +494,11 @@ void request_event()
 
   if (result == RESULT_NONE)
   {
-    int norm_pulses      = pulses - minimum_pulses;
-    int norm_max_pulses  = maximum_pulses - minimum_pulses;
-
-    buffer[0] = (byte)( norm_pulses           & 0xFF);
-    buffer[1] = (byte)((norm_pulses     >> 8) & 0xFF);
-    buffer[2] = (byte)( norm_max_pulses       & 0xFF);
-    buffer[3] = (byte)((norm_max_pulses >> 8) & 0xFF);
-    buffer[4] = (state == STATE_CALIBRATED) ? STATUS_CALIBRATED : STATUS_UNCALIBRATED;
+    buffer[0] = (byte)( pulses               & 0xFF);
+    buffer[1] = (byte)((pulses         >> 8) & 0xFF);
+    buffer[2] = (byte)( maximum_pulses       & 0xFF);
+    buffer[3] = (byte)((maximum_pulses >> 8) & 0xFF);
+    buffer[4] = isCalibrated ? 0x01 : 0x00;
     buffer[5] = crc(buffer, 5);
 
     Wire.write(buffer, 6);
