b6ef83e8d1d2582ddd726d35afb92880f1ba3903
mprhode <matildarhode@hotmail.com>
2018-07-17 14:38:13 -0600

    reworked for new environment

---
+++ nengo_foosball/simulator/agent.py
@@ -0,0 +1,163 @@
+from __future__ import print_function
+from __future__ import division
+import copy
+import numpy as np
+import time
+import random
+
+
+class Agent0(object):
+
+    def __init__(self, env, goal_left, recursion_depth=3, randomness=True):
+        """
+        env: should be Foosball environment object.
+        goal_left: True if player goal is on left side (note sure).
+        recursion_depth: How many wall bounces into the future to consider.
+        """
+        self.env = env
+        self.goal_left = goal_left
+        self.recursion_depth = recursion_depth
+        self.randomness = randomness
+        # get wall lines and bounds.
+        self.upper_bound = self.env.ball_radius
+        self.lower_bound = self.env.height - self.env.ball_radius
+        self.left_bound = self.env.ball_radius
+        self.right_bound = self.env.width - self.env.ball_radius
+        self.top_wall = self.line([self.left_bound, self.upper_bound], [self.right_bound, self.upper_bound])
+        self.bottom_wall = self.line([self.left_bound, self.lower_bound], [self.right_bound, self.lower_bound])
+        self.left_wall = self.line([self.left_bound, self.upper_bound], [self.left_bound, self.lower_bound])
+        self.right_wall = self.line([self.right_bound, self.upper_bound], [self.right_bound, self.lower_bound])
+        # get player lines and bounds.
+        self.players = [player for player in self.env.players if player.goal_left == goal_left]
+        self.player_lines = []
+        for player in self.players:
+            self.player_lines.append(self.line([player.x, self.upper_bound], [player.x, self.lower_bound]))
+        # --------------------------
+        self.previous_velocity = np.zeros(2, dtype=np.float32)
+        self.target_positions = [None, None, None, None]  # target y positions for each player (where the ball will be)
+
+    def line(self, p1, p2):
+        A = (p1[1] - p2[1])
+        B = (p2[0] - p1[0])
+        C = (p1[0] * p2[1] - p2[0] * p1[1])
+        return A, B, -C
+
+    def intersection(self, L1, L2):
+        D = L1[0] * L2[1] - L1[1] * L2[0]
+        Dx = L1[2] * L2[1] - L1[1] * L2[2]
+        Dy = L1[0] * L2[2] - L1[2] * L2[0]
+        if D != 0:
+            x = Dx / D
+            y = Dy / D
+            return x, y
+        else:
+            return False
+
+    def bounce(self, ball_pos, ball_vel):
+        """
+        Given a wall intersection point and a current ball velocity, return
+        a new ball velocity.
+        """
+        new_vel = copy.deepcopy(ball_vel)
+        new_pos = list(copy.deepcopy(ball_pos))
+        if ball_pos[0] <= self.left_bound + 0.5:
+            new_vel[0] *= -0.6
+            new_pos[0] = self.left_bound + 0.5
+        if ball_pos[1] <= self.upper_bound + 0.5:
+            new_vel[1] *= -0.6
+            new_pos[1] = self.upper_bound + 0.5
+        if ball_pos[0] >= self.right_bound - 0.5:
+            new_vel[0] *= -0.6
+            new_pos[0] = self.right_bound - 0.5
+        if ball_pos[1] >= self.lower_bound - 0.5:
+            new_vel[1] *= -0.6
+            new_pos[1] = self.lower_bound - 0.5
+        return new_vel, new_pos
+
+    def predict_player_intersections(self, ball_pos, ball_vel, ref_time, rec_depth=0):
+        """
+        Takes ball position ``ball_pos``, ball velocity ``ball_vel``, and current time ``ref_time`` as input.
+        Returns list of predicted player intersection y-coordinates along with the predicted time of said event.
+        Format: [<player_0>, <player_1>, <player_2>, <player_4>] where <player...> is a tuple (<y-position>, <direction>, <time>)
+        Returns ``None`` when there is no predicted intersection for a given player.
+        Example: [None, None, None, (20.0, True, 259.0)]; ball intersects player 4 line at y=20, towards our goal, at time 259.
+        Note: ^^^ predicted time not yet implemented.
+        """
+        if rec_depth >= self.recursion_depth:  # recursion depth exceeded.
+            print('Warning: recursion depth exceeded.')
+            return [None, None, None, None]
+        # Calculate ball vector line from position and velocity
+        ball_line = self.line([ball_pos[0], ball_pos[1]],
+                              [ball_pos[0] + ball_vel[0] * 1.0, ball_pos[1] + ball_vel[1] * 1.0])
+        # Calculate intersection between the ball vector line and player lines.
+        player_intersections = [None, None, None, None]
+        for idx, player_line in enumerate(self.player_lines):
+            if (ball_vel[0] > 0 and self.players[idx].x > ball_pos[0]) or (
+                    ball_vel[0] < 0 and self.players[idx].x < ball_pos[0]):
+                intersection = self.intersection(ball_line, player_line)
+                if intersection:
+                    x, y = intersection
+                    if x > self.left_bound and x < self.right_bound and y > self.upper_bound and y < self.lower_bound:
+                        player_intersections[idx] = (intersection[1], ball_vel[0] > 0, 0.0)
+        if not all([i is None for i in player_intersections]):
+            return player_intersections  # calculate intersection times and directions. TODO
+        else:  # if there are no player line intersections, then find intersection with wall.
+            wall0 = self.right_wall if ball_vel[
+                                           0] > 0 else self.left_wall  # consider ball vector direction when evaluating walls.
+            wall1 = self.bottom_wall if ball_vel[1] > 0 else self.top_wall
+            wall_intersections = [self.intersection(ball_line, wall) for wall in [wall0, wall1]]
+            wall_intersections = [intersection for intersection in wall_intersections if intersection not in [None , False]]
+            wall_collisions = [(x, y) for (x, y) in wall_intersections if
+                               x >= self.left_bound and x <= self.right_bound and y >= self.upper_bound and y <= self.lower_bound]
+            wall_collision_pos = wall_collisions[0] if len(wall_collisions) > 0 else None
+            if wall_collision_pos is None:
+                print('Warning: wall_collision_pos is None')
+                return [None, None, None, None]
+            new_vel, new_pos = self.bounce(wall_collision_pos, ball_vel)
+            return self.predict_player_intersections(new_pos, new_vel, 0, rec_depth=rec_depth + 1)  # TODO add ref_time
+
+    def servo_players(self):
+        """
+        Move paddles to target positions.
+        """
+        moves = [(1, 0), (5, 4), (9, 8), (13, 12)]  # (up, down)
+        for idx, target_y in enumerate(self.target_positions):
+            if target_y is not None:
+                # Select which guy to use as the servoing reference.
+                positions = [y + self.players[idx].offset for y in self.players[idx].ys]
+                candidate_ys = [idx_ for idx_, y in enumerate(self.players[idx].ys) if
+                                target_y >= y and target_y <= y + self.players[idx].max_y]
+                if len(candidate_ys) > 0:
+                    ordered = sorted([(abs(positions[idx_] - target_y), idx_) for idx_ in candidate_ys])
+                    position = positions[ordered[0][1]]  # Select the closest guy.
+                    if position > target_y + 1.0 and self.players[idx].offset > 0:
+                        return moves[idx][0]  # move up
+                    elif position < target_y - 1.0 and self.players[idx].offset < self.players[idx].max_y:
+                        return moves[idx][1]  # move down
+        return 8  # arbitrary action.
+
+    def select_action(self):
+        """
+        Select an action based on predictions of ball movement.
+        """
+        if np.array_equal(self.env.ball_vel, self.previous_velocity):  # only recalculate if ball velocity changes.
+            return self.servo_players()
+        #print('recalculating', time.time())
+        self.previous_velocity = copy.deepcopy(self.env.ball_vel)
+        player_intersections = self.predict_player_intersections(self.env.ball_pos, self.env.ball_vel,
+                                                                 0.01)  # Note: Hardcoded time.
+        for idx, intersection in enumerate(player_intersections):
+            if intersection is not None:
+                (y, moving_right, ts) = intersection
+                if moving_right == self.goal_left:  # Slightly confusing.
+                    # move intersecting players into the path of the ball.
+                    self.target_positions[idx] = intersection[0]  # set y position as target.
+                else:  # ball heading away from our goal.
+                    self.target_positions[idx] = intersection[0] + 200.0 / len(
+                        self.players[idx].ys)  # move intersecting players out of the way.
+        if self.randomness:
+            self.target_positions = [None if pos is None else (pos + (random.random() - 0.5) * 20.0) for pos in
+                                     self.target_positions]
+        # ---------------------------------------------------------
+        move = self.servo_players()
+        return move
--- nengo_foosball/simulator/reinforcement_q_learning_pytorch.py
+++ nengo_foosball/simulator/reinforcement_q_learning_pytorch.py
@@ -5,28 +5,15 @@ import numpy as np
 from collections import namedtuple
 from itertools import count
 
+import pygame
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-import torchvision.transforms as T
 
-from environment import Foosball
-
-assert torch.__version__ == '0.4.0'
-
-
-env = Foosball()
-
-# if gpu is to be used
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-Transition = namedtuple('Transition',
-                        ('state', 'blue_action', 'yellow_action', 'next_state', 'blue_reward', 'yellow_reward'))
-
+from old_environment import Foosball
 
 class ReplayMemory(object):
-
     def __init__(self, capacity):
         self.capacity = capacity
         self.memory = []
@@ -45,53 +32,18 @@ class ReplayMemory(object):
     def __len__(self):
         return len(self.memory)
 
-class DQN_blue(nn.Module):
-    def __init__(self):
-        super(DQN_blue, self).__init__()
-        self.l1 = nn.Linear(36, 1000)
-        self.l2 = nn.Linear(1000, 500)
-        self.head = nn.Linear(500, 8*2)
-
-    def forward(self, x):
-        x = F.relu(self.l1(x))
-        x = F.relu(self.l2(x))
-        return self.head(x)
-
 class DQN_yellow(nn.Module):
     def __init__(self):
         super(DQN_yellow, self).__init__()
-        self.l1 = nn.Linear(36, 250)
-        self.head = nn.Linear(250, 8*2)
+        self.l1 = nn.Linear(6, 250)
+        self.goalie = nn.Linear(250, 3)
+        self.defend = nn.Linear(250, 3)
+        self.midfield = nn.Linear(250, 3)
+        self.striker = nn.Linear(250, 3)
 
     def forward(self, x):
         x = F.relu(self.l1(x))
-        return self.head(x)
-
-env.reset()
-
-BATCH_SIZE = 128
-GAMMA = 0.999
-EPS_START = 0.9
-EPS_END = 0.05
-EPS_DECAY = 200
-TARGET_UPDATE = 10
-
-blue_policy_net = DQN_blue().to(device)
-blue_target_net = DQN_blue().to(device)
-blue_target_net.load_state_dict(blue_policy_net.state_dict())
-blue_target_net.eval()
-
-yellow_policy_net = DQN_yellow().to(device)
-yellow_target_net = DQN_yellow().to(device)
-yellow_target_net.load_state_dict(yellow_policy_net.state_dict())
-yellow_target_net.eval()
-
-blue_optimizer = optim.RMSprop(blue_policy_net.parameters())
-yellow_optimizer = optim.RMSprop(yellow_policy_net.parameters())
-
-memory = ReplayMemory(10000)
-
-steps_done = 0
+        return self.goalie(x), self.defend(x), self.midfield(x), self.striker(x)
 
 def select_action(state, model):
     global steps_done
@@ -100,13 +52,17 @@ def select_action(state, model):
         math.exp(-1. * steps_done / EPS_DECAY)
     steps_done += 1
     if sample > eps_threshold:
+        inp = torch.Tensor(state, device=device).cuda()
         with torch.no_grad():
-            return model(torch.Tensor(state, device=device)).max(0)[1].view(1, 1)
+            return F.softmax(torch.stack(model(inp)), dim=1)
     else:
-        return torch.tensor([[random.randrange(16)]], device=device, dtype=torch.long)
+        return torch.tensor([[random.randrange(3)] for x in range(4)], device=device, dtype=torch.long)
 
 
 def optimize_model(color):
+
+    memory = blue_memory if color == "blue" else yellow_memory
+
     if len(memory) < BATCH_SIZE:
         return
     transitions = memory.sample(BATCH_SIZE)
@@ -116,37 +72,26 @@ def optimize_model(color):
 
     # Compute a mask of non-final states and concatenate the batch elements
     non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-                                          batch.next_state)), device=device, dtype=torch.uint8)
+                                          batch.next_state)), device=device, dtype=torch.uint8).to(device)
     non_final_next_states = torch.cat([s for s in batch.next_state
-                                                if s is not None])
-    state_batch = torch.cat(batch.state)
+                                                if s is not None]).to(device)
+    state_batch = torch.cat(batch.state).to(device)
 
     if color == "yellow":
-        action_batch = torch.cat(batch.yellow_action)
-        reward_batch = torch.cat(batch.yellow_reward)
         policy_net = yellow_policy_net
         optimizer = yellow_optimizer
         target_net = yellow_target_net
-    elif color == "blue":
-        action_batch = torch.cat(batch.blue_action)
-        reward_batch = torch.cat(batch.blue_reward)
-        policy_net = blue_policy_net
-        optimizer = blue_optimizer
-        target_net = blue_target_net
 
-
-    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-    # columns of actions taken
+    action_batch = torch.cat(batch.action).to(device)
+    reward_batch = torch.cat(batch.reward).to(device)
 
     state_action_values = policy_net(state_batch).gather(1, action_batch) #MRED
 
-    # Compute V(s_{t+1}) for all next states.
     next_state_values = torch.zeros(BATCH_SIZE, device=device)
     next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach() #MRED
-    # Compute the expected Q values
+
     expected_state_action_values = (next_state_values * GAMMA) + reward_batch
 
-    # Compute Huber loss
     loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
 
     # Optimize the model
@@ -157,68 +102,95 @@ def optimize_model(color):
     optimizer.step()
 
 
-num_episodes = 50
-episode_durations = []
-for i_episode in range(num_episodes):
-    # Initialize the environment and state
+if __name__ == "__main__":
+    draw = False
+
+    env = Foosball(draw=draw)
+
+    # if gpu is to be used
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    Transition = namedtuple('Transition',
+                            ('state', 'action', 'next_state', 'reward'))
     env.reset()
-    
-    state = env.get_state() / 1000 # scale by 1000 for NN
 
-    all_actions = []
+    BATCH_SIZE = 128
+    GAMMA = 0.999
+    EPS_START = 0.9
+    EPS_END = 0.05
+    EPS_DECAY = 200
+    TARGET_UPDATE = 10
 
-    for t in count():
-        # Select and perform an action
-        b_action = select_action(state, blue_policy_net)
-        y_action = select_action(state, yellow_policy_net)
+    yellow_policy_net = DQN_yellow().to(device)
+    yellow_target_net = DQN_yellow().to(device)
+    yellow_target_net.load_state_dict(yellow_policy_net.state_dict())
+    yellow_target_net.eval()
 
-        action = np.zeros(16)
+    yellow_optimizer = optim.RMSprop(yellow_policy_net.parameters())
 
-        b, y = int(b_action[0][0]), int(y_action[0][0])
+    yellow_memory = ReplayMemory(10000)
 
-        unit = 100
+    steps_done = 0
 
-        if b_action % 2 != 0:
-            # convert to negative
-            action[(b - 1)//2] = -unit
-        else:
-            action[b//2] = unit
+    yellow_policy_net = yellow_policy_net
+    yellow_target_net = yellow_target_net
 
-        if y_action % 2 != 0:
-            # convert to negative
-            action[8 + (y - 1)//2] = -unit
-        else:
-            action[8 + y//2] = unit
+    num_episodes = 50
+    episode_durations = []
+    for i_episode in range(num_episodes):
+        # Initialize the environment and state
+        env.reset()
 
-        all_actions.append(np.round(action))
-        next_state, reward, done = env.step(action)
-        next_state /= 1000
+        state, acttime = env.get_state()
+        all_actions = [(np.array(([0]* (4+4*4+4*4) + [acttime])))]
 
-        # Store the transition in memory
-        memory.push(torch.Tensor(state, device=device).unsqueeze(0), 
-            b_action, y_action, 
-            torch.Tensor(next_state, device=device).unsqueeze(0), 
-            torch.Tensor([reward[0]], device=device),  
-            torch.Tensor([reward[1]], device=device)
-        )
+        state = state / 1000 # scale by 1000 for NN
 
-        # Move to the next state
-        state = next_state
+        old_rewards = [0,0]
 
-        # Perform one step of the optimization (on the target network)
-        optimize_model("yellow")
-        optimize_model("blue")
+        for t in count():
+            # Select and perform an action
 
-        if done:
+            action = select_action(state, yellow_policy_net)
+
+            next_state, yreward, done, score = env.step(action)
+
+            all_actions.append(np.concatenate([action, np.array([acttime])]))
+            next_state /= 1000
+
+            yellow_memory.push(
+                torch.Tensor(state, device=device).unsqueeze(0),
+                action,
+                torch.Tensor(yellow(next_state), device=device).unsqueeze(0),
+                torch.Tensor([yreward], device=device)
+            )
+
+            # Move to the next state
+            state = next_state
+
+            if draw and t % 10 == 0:
+                env.draw()
+
+            # Perform one step of the optimization (on the target network)
+            optimize_model("yellow")
+
+            if done:
+                episode_durations.append(t + 1)
+                print("episode {}, score [B,Y] {}, dur {}".format(i_episode, reward, episode_durations[-1]))
+                break
+
+        # Update the target network
+        if i_episode % TARGET_UPDATE == 0:
             episode_durations.append(t + 1)
             print("episode {}, score [B,Y] {}, dur {}".format(i_episode, reward, episode_durations[-1]))
+            print(np.array(all_actions).shape)
             pickle.dump(np.array(all_actions), open("episode_{}_[B,Y] {}.p".format(i_episode, reward), "wb"))
-            break
+            yellow_target_net.load_state_dict(yellow_policy_net.state_dict())
+            torch.save(yellow_target_net, "yellow.pt")
 
-    # Update the target network
-    if i_episode % TARGET_UPDATE == 0:
-        yellow_target_net.load_state_dict(yellow_policy_net.state_dict())
-        blue_target_net.load_state_dict(blue_policy_net.state_dict())
+    torch.save(yellow_target_net, "yellow.pt")
 
-print('Complete')
+    print('Complete')
+
+
 
