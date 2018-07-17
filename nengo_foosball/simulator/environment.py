4bf3e2a61dec4db48dace34e6aa0d80c701da27c
mprhode <matildarhode@hotmail.com>
2018-07-17 00:14:30 -0600

    categorical and continuous step functions

--- nengo_foosball/simulator/environment.py
+++ nengo_foosball/simulator/environment.py
@@ -1,7 +1,6 @@
-import nengo
 import numpy as np
-#import pygame
-import time
+import pygame
+from agent import Agent0
 
 WHITE = (255,255,255)
 GREEN = (0,171,93)
@@ -15,16 +14,11 @@ BLUE = (25,139,232)
 width = 800
 height = 400
 
-display = False
-
-if display:
-    canvas = pygame.display.set_mode((width, height), 0, 32)
-else: 
-    canvas = 1
+canvas = None
 
 
 class Player(object):
-    def __init__(self, x, ys, max_y, color, goal_left):
+    def __init__(self, x, ys, max_y, color, goal_left, display=True):
         self.x = x
         self.ys = ys
         self.max_y = max_y
@@ -40,8 +34,22 @@ class Player(object):
         self.slide_velocity = 0
         self.rot_off = 0
         self.colors = [YELLOW, DARK_YELLOW] if self.color == "yellow" else [BLUE, DARK_BLUE]
+        self.old_score = np.zeros(2)
+        self.display = display
 
-        
+    def act(self, action):
+        # 0 = left, 1 = right, 2 = kick
+        self.neg = 0
+        if action == 0:
+            self.slide(0.05, -500)
+        elif action == 1:
+            self.slide(0.05, 500)
+        elif action == 2:
+            self.rotate(0.25, -100)
+            self.rotate(0.25, 100)
+            self.rotate(0.01, -50)
+            self.neg = 1 - self.neg
+
     def slide(self, dt, velocity):
         self.slide_velocity = velocity
         self.offset = np.clip(self.offset + self.slide_velocity*dt, 0, self.max_y)
@@ -72,11 +80,6 @@ class Player(object):
         
     def __opacity(self):
         if self.rotate_offset < self.kick_deg or self.rotate_offset > 270 - self.kick_deg:
-
-        #if self.goal_left and self.rotate_offset > 0 and self.rotate_offset <= 180:
-        #    return 0.5 
-        #elif not(self.goal_left) and self.rotate_offset > 180:
-        
             return 1
         return 0.5
         
@@ -93,7 +96,7 @@ class Player(object):
         '''.format(x0=self.x-line_width/2, players=''.join(players), line_width=line_width, height=self.table.height)
 
     def draw(self):
-        if display:
+        if self.display:
             line_width = 5
             pygame.draw.rect(canvas, GREY, [self.x-line_width/2, 0, line_width, self.table.height])
 
@@ -108,52 +111,57 @@ class Player(object):
                 ])
 
 class Foosball(object):
-    def __init__(self, draw=False):
+    def __init__(self, display=True):
         self.width = width
         self.height = height
         self.ball_radius = 12
-                
+
         self.players = []
 
         self.add_player(Player(x=50, 
                            ys=[self.height/3], 
                            max_y=self.height/3,
-                           color="blue", goal_left=False))
+                           color="blue", goal_left=False, display=display))
         self.add_player(Player(x=150, 
                                    ys=[0, self.height/2], 
                                    max_y=self.height/2,
-                                   color="blue", goal_left=False))
+                                   color="blue", goal_left=False, display=display))
         self.add_player(Player(x=350, 
                                    ys=np.linspace(0, self.height-self.height/3+25, 5), 
                                    max_y=self.height/3-25,
-                                   color="blue", goal_left=False))
+                                   color="blue", goal_left=False, display=display))
         self.add_player(Player(x=550, 
                                    ys=np.linspace(0, self.height-self.height/3-25, 3), 
                                    max_y=self.height/3+25,
-                                   color="blue", goal_left=False))
+                                   color="blue", goal_left=False, display=display))
                                    
                                    
         self.add_player(Player(x=750, 
                                    ys=[self.height/3], 
                                    max_y=self.height/3,
-                                   color="yellow", goal_left=True))
+                                   color="yellow", goal_left=True, display=display))
         self.add_player(Player(x=800-150, 
                                    ys=[0, self.height/2], 
                                    max_y=self.height/2,
-                                   color="yellow", goal_left=True))
+                                   color="yellow", goal_left=True, display=display))
         self.add_player(Player(x=800-350, 
                                    ys=np.linspace(0, self.height-self.height/3+25, 5), 
                                    max_y=self.height/3-25,
-                                   color="yellow", goal_left=True))
+                                   color="yellow", goal_left=True, display=display))
         self.add_player(Player(x=800-550, 
                                    ys=np.linspace(0, self.height-self.height/3-25, 3), 
                                    max_y=self.height/3+25,
-                                   color="yellow", goal_left=True))
+                                   color="yellow", goal_left=True, display=display))
 
         self.reset()
 
-        if draw:
+        self.display = display
+        if self.display:
+            global canvas
+            canvas = pygame.display.set_mode((width, height), 0, 32)
             self.draw()
+
+        self.agent = Agent0(self, False)
 
     def add_player(self, player):
         player.table = self
@@ -162,52 +170,109 @@ class Foosball(object):
     def reset(self):
         self.reset_table()
         self.score = np.zeros(2)
+        self.old_score = np.zeros(2)
 
     def reset_table(self):
         self.ball_pos = np.array([self.width/2, self.height/2])
-        self.ball_vel = np.random.uniform(-100, 100, 2) # ball velocity at reset
+        self.ball_vel = np.random.uniform(-500, 500, 2) # ball velocity at reset
         for p in self.players:
             p.reset_offset = 0
             p.slide_velocity = 0
             p.rotate_offset = 0 
             p.rot_velocity = 0
         
-    def update(self, dt, slide):
-        self.ball_pos += self.ball_vel*dt  
+    def update(self, dt, yaction, categorical=False):
+        """
+        dt = 0.01
+        action = [goalie slide, goalie rotate,
+        defenders slide, defenders rotate,
+        midfield slide, midfield rotate,
+        strikers slide, strikers rotate] to control YELLOW team
+        """
+        self.ball_pos += self.ball_vel*dt
         
         if self.ball_pos[0]-self.ball_radius < 0 and self.height/3 < self.ball_pos[1] < 2*self.height/3:
             self.score[1] += 1
-            print("goal yellow")
+            print("goal scored by yellow")
             self.reset_table()
         if self.ball_pos[0]+self.ball_radius > self.width and self.height/3 < self.ball_pos[1] < 2*self.height/3:
             self.score[0] += 1
-            print("goal blue")
+            print("goal scored by blue")
             self.reset_table()
+
+        wall_slow_down = 0.6
         
         if self.ball_pos[0]-self.ball_radius < 0:
             self.ball_pos[0] = self.ball_radius
-            self.ball_vel[0] *= -1
+            self.ball_vel[0] *= -wall_slow_down
         if self.ball_pos[1]-self.ball_radius < 0:
             self.ball_pos[1] = self.ball_radius
-            self.ball_vel[1] *= -1
+            self.ball_vel[1] *= -wall_slow_down
         if self.ball_pos[0]+self.ball_radius > self.width:
             self.ball_pos[0] = self.width - self.ball_radius
-            self.ball_vel[0] *= -1
+            self.ball_vel[0] *= -wall_slow_down
         if self.ball_pos[1]+self.ball_radius > self.height:
             self.ball_pos[1] = self.height - self.ball_radius
-            self.ball_vel[1] *= -1
+            self.ball_vel[1] *= -wall_slow_down
+
+        b = self.agent.select_action()
+        baction = np.zeros(8)
+        unit = 150
+        if b % 2 != 0:
+            # convert to negative
+            baction[(b - 1) // 2] = -unit
+        else:
+            baction[b // 2] = unit
             
         for i, p in enumerate(self.players):
-            p.slide(dt=dt, velocity=slide[i*2])
-            p.rotate(dt=dt, velocity=slide[i*2+1])
+            if i >= 4:
+                if categorical:
+                    p.act(yaction[i - 4])
+                else:
+                    p.slide(dt=dt, velocity=yaction[(i - 4) * 2])
+                    p.rotate(dt=dt, velocity=yaction[(i - 4) * 2 + 1])
+            else:
+                p.slide(dt=dt, velocity=baction[i*2])
+                p.rotate(dt=dt, velocity=baction[i*2+1])
+
             cx, cy = p.collide(self.ball_pos[0], self.ball_pos[1], ball_radius=self.ball_radius)
+
             self.ball_pos += [cx, cy]
+
             new_v = np.array([cx, cy])
+
             v_norm = np.linalg.norm(new_v)
             if v_norm > 0:
                 mag = np.linalg.norm(self.ball_vel)
                 self.ball_vel = mag * new_v / v_norm + p.rot_velocity
-        
+
+            if self.display:
+                self.draw()
+
+    def get_full_state(self):
+        # includes human players - should only be used for blue team decisions
+        """ action_vector = [
+                    ball_pos_x, ball_velocity_x,
+                    ball_pos_y, ball_veolcity_y
+
+                     b_s_goalie_pos, b_s_goalie_vel,
+                     b_r_goalie_pos, b_r_goalie_vel,
+                     b_s_defenders_pos, b_s_defenders_vel,
+                     b_r_defenders_pos, b_r_defenders_vel,
+                     b_s_midfield_pos, b_s_midfield_vel,
+                     b_r_midfield_pos, b_r_midfield_vel,
+                     b_s_strikers_pos, b_s_strikers_vel,
+                     b_r_strikers_pos, b_r_strikers_vel,
+
+                    + REPEAT for yellows
+
+                ]"""
+
+        return np.concatenate([[self.ball_pos[0], self.ball_vel[0], self.ball_pos[1], self.ball_vel[1]],
+            np.array([[p.offset, p.slide_velocity, p.rotate_offset, p.rot_velocity] for p in
+                self.players]).flatten()
+            ])
+
     def svg(self):
 
         template = '''<svg width="100%" height="100%" viewbox="0 0 {width} {height}">
@@ -235,7 +300,7 @@ class Foosball(object):
         return template
 
     def draw(self):
-        if display:
+        if self.display:
            
             canvas.fill(GREEN)
 
@@ -266,35 +331,34 @@ class Foosball(object):
             pygame.display.update()        
         
     def get_state(self):
-        # b = blue, y = yellow, s = slide, r = rotate
-        """ action_vector = [   
-            ball_pos_x, ball_velocity_x, 
-            ball_pos_y, ball_veolcity_y
+        """only ball position and yellow players y-position and rotate position returned"""
+        players = [p.offset for p in self.players[4:]]
+        return np.concatenate([self.ball_pos, players]).flatten()
 
-             b_s_goalie_pos, b_s_goalie_vel,
-             b_r_goalie_pos, b_r_goalie_vel,
-             b_s_defenders_pos, b_s_defenders_vel,
-             b_r_defenders_pos, b_r_defenders_vel,             
-             b_s_midfield_pos, b_s_midfield_vel,
-             b_r_midfield_pos, b_r_midfield_vel,             
-             b_s_strikers_pos, b_s_strikers_vel,
-             b_r_strikers_pos, b_r_strikers_vel,
+    def step(self, action_vector, categorical=False):
+        """ actions are slide and rotation velocity for each stick starting with goalie (8 actions)
 
-            + REPEAT for yellows
-             
-        ]"""
+        returns state (ball x, ball y and y-pos of sliders), reward, done, score
 
-        return np.concatenate([[self.ball_pos[0], self.ball_vel[0], self.ball_pos[1], self.ball_vel[1]],
-                                    np.array([[p.offset, p.slide_velocity, p.rotate_offset, p.rot_velocity] for p in self.players]).flatten()
-                                ]), time.time()
+        """
+        assert categorical == False and len(action_vector) == 8 or categorical == True and len(action_vector) == 4, \
+            "Cannot use action vector of length {} with categorical={}".format(len(action_vector), categorical)
 
+        self.update(0.05, action_vector, categorical=categorical)
 
-    def step(self, action_vector):
-       
-        self.update(0.01, action_vector)
-        
-        return self.get_state(), self.score, max(self.score) >= 10
+        #reward = change in reward +1 for goal to blue, -1 for goal to yellow
+        reward = 0
+        if self.score[0] > self.old_score[0]:
+            reward = 1
+            self.old_score = self.score
+        elif self.score[1] > self.old_score[1]:
+            reward = -1
+            self.old_score = self.score
+
+        return self.get_state(), reward, max(self.score) >= 10, self.score
 
 if __name__ == "__main__":
     f = Foosball()
-
+    for t in range(1000):
+        f.step(np.random.uniform(-100, 100, 8), categorical=False)
+        f.step([np.random.randint(3) for x in range(4)], categorical=True)
